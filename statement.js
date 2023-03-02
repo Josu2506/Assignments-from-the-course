79174cfdc32766fbe05192e5aebe5f79375a8c18
Josu Adan Legarra <54106858+Josu2506@users.noreply.github.com>
2023-03-02 20:35:31 +0100

    Add files via upload

---
+++ statement.js
@@ -0,0 +1,26 @@
+var day = 'Friday';
+switch (day) {
+    case 'Monday':
+    console.log('Gym');
+    break;
+    case 'Tuesday':
+    console.log('Study');
+    break;
+    case 'Wednesday':
+    console.log('Gym');
+    break;
+    case 'Thursday':
+    console.log('Keep studying');
+    break;
+    case 'Friday':
+    console.log('Third day of gym');
+    break;
+    case 'Saturday':
+    console.log('Futsal game on the morning');
+    break;
+    case 'Sunday':
+    console.log('Rest and chill');
+    break;
+    default:
+    console.log('There is no such day');
+}
