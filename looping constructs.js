975866536e1ba387be882516a98bb03317e22c4a
Josu Adan Legarra <54106858+Josu2506@users.noreply.github.com>
2023-03-06 23:11:46 +0100

    Add files via upload

---
+++ looping constructs.js
@@ -0,0 +1,38 @@
+// Practice For loop and While loop
+
+// FOR LOOP
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting complete');
+
+for (var i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log('Counting finished')
+
+// WHILE LOOP
+
+var i = 1 
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+var i = 5 
+
+while (i >= 1) {
+    console.log(i);
+    i = i - 1;
+}
+console.log('Counting finished!');
+
+var year = 2018
+
+while(year <= 2022) {
+    console.log(year);
+    year = year + 1;
+};
